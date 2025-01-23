ebe225376d72046f675294d2fba4843477c49b4f
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-23 14:35:05 +0100

    feat: migrate mysql migration to postgres

--- src/infrastructure/repositories/chain-state/kysely/migrations/3-create-vehicle-infos-table.ts
+++ src/infrastructure/repositories/chain-state/kysely/migrations/3-create-vehicle-infos-table.ts
@@ -3,16 +3,16 @@ import { Kysely, sql } from 'kysely';
 export async function up(database: Kysely<any>): Promise<void> {
   await database.schema
     .createTable('vehicle_infos')
-    .addColumn('id', 'integer', (col) => col.primaryKey().autoIncrement())
+    .addColumn('id', 'serial', (col) => col.primaryKey())
     .addColumn('vehicle_id', 'integer', (col) =>
       col.references('vehicle.id').onDelete('cascade').notNull()
     )
-    .addColumn('holder_count', 'integer', (col) => col.notNull())
+    .addColumn('holder_count', 'numeric', (col) => col.notNull())
     .addColumn('first_registration_in_france_date', 'varchar(255)', (col) => col.notNull())
     .addColumn('first_siv_registration_date', 'varchar(255)', (col) => col.notNull())
     .addColumn('license_plate', 'varchar(255)', (col) => col.notNull())
     .addColumn('siv_conversion_date', 'varchar(255)')
-    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull())
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
     .execute();
 }
 
